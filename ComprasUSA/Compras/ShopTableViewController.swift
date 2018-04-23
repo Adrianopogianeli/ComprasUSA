a51835ba902bdfe2736f0fcc9f2eb481def6a1fa
Adriano R P L <adriano.pogianeli@gmail.com>
2018-04-22 23:50:03 -0300

    Add Cell action on ShoppingTableViwer

--- ComprasUSA/Compras/ShopTableViewController.swift
+++ ComprasUSA/Compras/ShopTableViewController.swift
@@ -12,32 +12,94 @@ import CoreData
 class ShopTableViewController: UITableViewController {
     
     var label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 22))
-
+    var fetchedResultController: NSFetchedResultsController<Product>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadProdutos()
+        
         tableView.estimatedRowHeight = 106
         tableView.rowHeight = UITableViewAutomaticDimension
-        self.label.text = "Sua lista está vazia!"
-        self.label.textAlignment = .center
-        self.label.textColor = UIColor.black
-        tableView.backgroundView?.addSubview(self.label)
-        tableView.separatorStyle = .none
         
-        //loadMovies()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.text = "Sua lista está vazia!"
+        label.textAlignment = .center
+        label.textColor = .black
+        
+    }
+    
+    func loadProdutos() {
+        let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultController.delegate = self
+        do {
+            try fetchedResultController.performFetch()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 148
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let count = fetchedResultController.fetchedObjects?.count {
+            tableView.backgroundView = (count == 0) ? label : nil
+            return count
+        } else {
+            tableView.backgroundView = label
+            return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "prodCell", for: indexPath) as! ProdutoTableViewCell
+        let prod = fetchedResultController.object(at: indexPath)
+        
+        cell.lbNomedoProduto.text = prod.name
+        cell.lbValor.text =  prod.value.description
+        cell.lbEstado.text = prod.state?.name
+        cell.lbYesNo.text = prod.credcard ? "Sim" : "Não"
+        if let image = prod.image as? UIImage {
+            cell.ivTitulo.image = image
+        } else {
+            cell.ivTitulo.image = nil
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        let produto = fetchedResultController.object(at: indexPath)
+        context.delete(produto)
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let backButton = UIBarButtonItem()
-        backButton.title = "Back"
-        navigationItem.backBarButtonItem = backButton
+        if let vc = segue.destination as? AddProdViewController {
+            if tableView.indexPathForSelectedRow != nil {
+                vc.produto = fetchedResultController.object(at: tableView.indexPathForSelectedRow!)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+}
+extension ShopTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.reloadData()
     }
-
-
 }
 
